0c2a3d8fcde253bc23e82164b5ead1b990daa7d0
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-07-02 17:05:47 -0400

    Add controller

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+import { createUser } from '../models/userModel.js';
+import crypto from 'crypto';
+
+export async function insertUser(req, res) {
+    console.log(req.body);
+    const salt = crypto.randomBytes(16).toString('base64');
+    const hash = crypto.createHmac('sha512', salt).update(req.body.password).digest('base64');
+
+    req.body.password = `${salt}${hash}`;
+
+    try {
+        const document = await createUser(req.body);
+
+        res.status(201).send({ id: document._id });
+    } catch (error) {
+        res.status(400);
+    }
+}
