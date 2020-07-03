577da3a1de6589eee93a55e4c510a38ada7db1ec
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-07-03 04:21:51 -0400

    Add mongo connection

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 import router from './routes/index.js';
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+mongoose.connect(
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ds129651.mlab.com:29651/jss`,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
